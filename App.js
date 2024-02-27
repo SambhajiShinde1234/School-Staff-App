1c2e1b2e8abe7636f64da17df7b73de1f830c9c6
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-02-27 20:35:14 +0530

    add staff screen text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your appasdsaa!</Text>
+      <Text>staff screen</Text>
       <StatusBar style="auto" />
     </View>
   );
