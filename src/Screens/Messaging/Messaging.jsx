2bef447ad933dc36c1c93daf90ee87c261f9b222
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-03-04 23:27:16 +0530

    update messaging screen

--- src/Screens/Messaging/Messaging.jsx
+++ src/Screens/Messaging/Messaging.jsx
@@ -10,20 +10,8 @@ const Messaging = () => {
           contentContainerStyle={styles.scrollViewContent}
           showsVerticalScrollIndicator={false}
         >
-            <View style={styles.imgContainer}>
-                <Text style={styles.name}>Perry Thomas</Text>
-            </View>
-            {
-                PROFILE_DATA.map((item) => {
-                    return(
-                        <View key={item.id} style={styles.container}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <Text style={styles.details}>{item.name}</Text>
-                        </View>
-                    )
-                })
-            }
-           
+          
+        
         </ScrollView>
     </>
    
@@ -44,59 +32,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         backgroundColor:'#F8FDFF'
     },
-    imgContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        gap: 10,
-        alignItems: 'center',
-        borderColor: '#C0EDFF',
-        padding: 10,
-        borderRadius: 5,
-        marginBottom: 20
-    },
-    image: {
-        height: 100,
-        width: 100,
-        resizeMode:'contain',
-        borderRadius: 100,
-    },
-    iconHolder: {
-        borderWidth: 10,
-        borderColor: '#FFF',
-        borderRadius: 100,
-        width: 130,
-        height: 130,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderWidth: 2,
-        borderColor: '#FFF',
-        backgroundColor: '#FFF',
-        padding: 15,
-        borderRadius: 5,
-        marginBottom: 10
-    },
-    name: {
-        fontSize: 18,
-        color: '#313131',
-        fontWeight: 'bold'
-    },
-    title: {
-        fontSize: 14,
-        color: '#313131',
-    },
-    details: {
-        fontSize: 14,
-        color: '#313131',
-        fontWeight: 'bold'
-    }
 });
 
 export default Messaging;
